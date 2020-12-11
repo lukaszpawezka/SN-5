8728c6736f73fabd4f7ff6bfb6418f4b1c9dfbd2
lukaszpawezka <56358554+lukaszpawezka@users.noreply.github.com>
2020-12-11 10:08:12 +0100

    Add files via upload

---
+++ mlp.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+import gzip
+import pickle
+import matplotlib.ticker as ticker
+from tensorflow.keras import backend as K
+
+f = gzip.open('mnist.pkl.gz', 'rb')
+training_data, validation_data, test_data = pickle.load(f, encoding="latin1")
+f.close()
+
+
+
+
+
+initializer = tf.keras.initializers.HeNormal()
+
+model = models.Sequential()
+model.add(layers.Dense(60, input_shape=(784, ), activation='relu', kernel_initializer=initializer))
+model.add(layers.Dense(10, activation='relu', kernel_initializer=initializer))
+model.add(layers.Softmax())
+
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+epoch = 10
+
+history = model.fit(training_data[0],
+                    training_data[1],
+                    epochs=epoch,
+                    batch_size=100,
+                    validation_data=(validation_data[0], validation_data[1]))
+
+
+plt.title("MLP")
+plt.plot(np.arange(0, epoch, 1), history.history['accuracy'], "o-", label='Dokładność')
+plt.plot(np.arange(0, epoch, 1), history.history['val_accuracy'], "o-", label='Dokładność na zbiorze walidacyjnym')
+plt.xlabel('Epoki')
+plt.ylabel('Dokładność')
+plt.xticks(np.arange(1,epoch, 1))
+plt.legend(loc='lower right')
+plt.show()
+
+test_loss, test_acc = model.evaluate(test_data[0],  test_data[1], verbose=2)
+
+print(test_acc)
